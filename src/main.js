a2b018918a962509ef4984dc4c1e24e6e3e9bcb7
Grazziano Fagundes <grazzianofagundes@gmail.com>
2022-10-19 20:06:08 -0300

    Obtendo e exibindo o número do cartão

--- src/main.js
+++ src/main.js
@@ -99,3 +99,14 @@ function updateSecurityCode(code) {
   const ccSecurity = document.querySelector(".cc-security .value")
   ccSecurity.innerText = code.length === 0 ? "123" : code
 }
+
+cardNumberMasked.on("accept", () => {
+  const cardType = cardNumberMasked.masked.currentMask.cardtype
+  setCardType(cardType)
+  updateCardNumber(cardNumberMasked.value)
+})
+
+function updateCardNumber(number) {
+  const ccNumber = document.querySelector(".cc-number")
+  ccNumber.innerText = number.length === 0 ? "1234 5678 9012 3456" : number
+}
